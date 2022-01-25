6da7c977f351e2ec9fbc64da8445e7b6205c3248
jpbpissineli <87237106+jpbpissineli@users.noreply.github.com>
2022-01-25 14:13:03 -0300

    Update receita.php

--- site_trabalho2/receita.php
+++ site_trabalho2/receita.php
@@ -17,19 +17,19 @@ endif;
 
 // dados usuário
 $id_usuario = $_SESSION['id_usuario'];
-$sql = "SELECT * FROM usuario WHERE CodigoUsu = '$id_usuario'";
+$sql = "SELECT * FROM usuario WHERE codusu = '$id_usuario'";
 $resultado = mysqli_query($connect, $sql);
 $dados = mysqli_fetch_assoc($resultado);
 
 // dados receita
 if ($_SESSION['post']):
 	$id_receita = $_SESSION['id_receita'];
-	$sql = "SELECT * FROM receita WHERE CodReceita = '$id_receita'";
+	$sql = "SELECT * FROM receita WHERE codreceita = '$id_receita'";
 	$resultado = mysqli_query($connect, $sql);
 	$dados_receita =mysqli_fetch_assoc($resultado);
 else:
 	$id_receita = $_GET['id_receita'];
-	$sql = "SELECT * FROM receita WHERE CodReceita = '$id_receita'";
+	$sql = "SELECT * FROM receita WHERE codreceita = '$id_receita'";
 	$resultado = mysqli_query($connect, $sql);
 	$dados_receita = mysqli_fetch_assoc($resultado);
 endif;
@@ -42,7 +42,7 @@ endif;
 		<div class="nav-wrapper container"><a id="logo-container" href="home.php" class="brand-logo left">ComeCome</a>
 		  <ul class="right">
 			<li><a href="post.php" class="btn-floating #f57f17 yellow darken-4"> <i class= "material-icons"> add_circle </i> </a> </li>
-			<li><a href="perfil.php?id_usuario=<?php $meuperfil = true; echo $id_usuario.'&meuperfil='.$meuperfil;?>" class="btn-floating #f57f17 yellow darken-4"> <i class= "material-icons"> account_circle </i> </a> </li>
+			<li><a href="perfil.php?id_usuario=<?php $meuperfil = true; echo $id.'&meuperfil='.$meuperfil;?>" class="btn-floating"> <img class="circle z-depth-2" height='50px' width='50px' src="fotosperfil/<?php echo $dados['imagem']; ?>"> </a> </li>
 			<li><a href="logout.php" class="btn-floating #f57f17 yellow darken-4"> <i class= "material-icons"> stop </i> </a> </li>
 		  </ul>
 		</div>
@@ -59,10 +59,10 @@ endif;
 			
 			$servidor = $_SERVER['PHP_SELF'];
 			
-			$sql = "SELECT fk_Usuario_CodigoUsu FROM receita WHERE CodReceita = '$id_receita'";
+			$sql = "SELECT autor FROM receita WHERE codreceita = '$id_receita'";
 			$resultado = mysqli_query($connect, $sql);
 			$array = mysqli_fetch_assoc($resultado);
-			$id_donodareceita = $array['fk_Usuario_CodigoUsu'];
+			$id_donodareceita = $array['autor'];
 			
 			if($id_usuario == $id_donodareceita ):
 				echo "<div class='fotonome'>
@@ -94,24 +94,24 @@ endif;
 			  <div class="card s12">
 				<div class="card-image fotonome">
 				  <img class='responsive-img' src="arquivos/<?php echo $dados_receita['imagem']; ?>">
-				  <h3><?php echo $dados_receita['NomeRec'];?></h3>
+				  <h3><?php echo $dados_receita['nomerec'];?></h3>
 				</div>
 				<div class='card-content'>
 					<div class="divider"></div>
 					<div class="section">
-						<h5>Descrição:<br></h5><p><?php echo $dados_receita['descricao']; ?></p>
+						<h5>Descrição:<br></h5><p><?php echo $dados_receita['sobre']; ?></p>
 					</div>
 				</div>
 				<div class='card-content'>
 					<div class="divider"></div>
 					<div class="section">
-						<h5>Ingredientes:<br></h5><p><?php echo $dados_receita['ingredientes']; ?></p>
+						<h5>Ingredientes:<br></h5><p><?php echo $dados_receita['ingrediente']; ?></p>
 					</div>
 				</div>
 				<div class='card-content'>
 					<div class="divider"></div>
 					<div class="section">
-						<h5>Modo de preparo:</h5><br><p><?php echo $dados_receita['Preparo']; ?></p>
+						<h5>Modo de preparo:</h5><br><p><?php echo $dados_receita['preparo']; ?></p>
 					</div>
 				</div>
 			  </div>
